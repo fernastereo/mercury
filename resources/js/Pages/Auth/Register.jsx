761986ad327fb6afa402593b632ed920ce6d01aa
Fernando Cueto <fernandoecueto@gmail.com>
2023-02-25 01:59:36 +0100

    implement new design in register layout

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,40 +1,55 @@
-import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
     });
 
     useEffect(() => {
         return () => {
-            reset('password', 'password_confirmation');
+            reset("password", "password_confirmation");
         };
     }, []);
 
     const handleOnChange = (event) => {
-        setData(event.target.name, event.target.type === 'checkbox' ? event.target.checked : event.target.value);
+        setData(
+            event.target.name,
+            event.target.type === "checkbox"
+                ? event.target.checked
+                : event.target.value
+        );
+    };
+
+    const handleSocialLogin = (event) => {
+        event.preventDefault();
+
+        //TODO: Implement social login
+        console.log("Social login is not implemented yet");
     };
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('register'));
+        post(route("register"));
     };
 
     return (
         <GuestLayout>
             <Head title="Register" />
 
-            <form onSubmit={submit}>
+            <form
+                className="font-ubuntu bg-white rounded-md shadow-2xl p-5"
+                onSubmit={submit}
+            >
                 <div>
                     <InputLabel htmlFor="name" value="Name" />
 
@@ -87,7 +102,10 @@ export default function Register() {
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+                    <InputLabel
+                        htmlFor="password_confirmation"
+                        value="Confirm Password"
+                    />
 
                     <TextInput
                         id="password_confirmation"
@@ -100,20 +118,78 @@ export default function Register() {
                         required
                     />
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
+                    <InputError
+                        message={errors.password_confirmation}
+                        className="mt-2"
+                    />
                 </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Already registered?
-                    </Link>
+                <Link
+                    href={route("login")}
+                    className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Already registered?
+                </Link>
 
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
+                <button
+                    className="block w-full text-center bg-mercury-1 mt-3 py-2 rounded-2xl hover:bg-mercury-2 hover:-translate-y-1 transition-all duration-500 text-black font-semibold mb-3"
+                    disabled={processing}
+                >
+                    Register
+                </button>
+
+                <div className="flex items-center justify-center space-x-2 my-4">
+                    <span className="h-px w-16 bg-gray-200"></span>
+                    <span className="text-gray-400 font-ubuntu">or</span>
+                    <span className="h-px w-16 bg-gray-200"></span>
+                </div>
+                <div className="flex justify-center gap-5 w-full mb-4">
+                    <button
+                        onClick={handleSocialLogin}
+                        className="w-full flex items-center justify-center mb-6 md:mb-0 border border-gray-300 hover:border-mercury-1 hover:bg-gray-200 text-sm text-gray-500 p-3 rounded-lg tracking-wide font-medium cursor-pointer hover:-translate-y-1 transition-all duration-500"
+                    >
+                        <svg
+                            className="w-4 mr-2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill="#EA4335"
+                                d="M5.266 9.765A7.077 7.077 0 0 1 12 4.909c1.69 0 3.218.6 4.418 1.582L19.91 3C17.782 1.145 15.055 0 12 0 7.27 0 3.198 2.698 1.24 6.65l4.026 3.115Z"
+                            />
+                            <path
+                                fill="#34A853"
+                                d="M16.04 18.013c-1.09.703-2.474 1.078-4.04 1.078a7.077 7.077 0 0 1-6.723-4.823l-4.04 3.067A11.965 11.965 0 0 0 12 24c2.933 0 5.735-1.043 7.834-3l-3.793-2.987Z"
+                            />
+                            <path
+                                fill="#4A90E2"
+                                d="M19.834 21c2.195-2.048 3.62-5.096 3.62-9 0-.71-.109-1.473-.272-2.182H12v4.637h6.436c-.317 1.559-1.17 2.766-2.395 3.558L19.834 21Z"
+                            />
+                            <path
+                                fill="#FBBC05"
+                                d="M5.277 14.268A7.12 7.12 0 0 1 4.909 12c0-.782.125-1.533.357-2.235L1.24 6.65A11.934 11.934 0 0 0 0 12c0 1.92.445 3.73 1.237 5.335l4.04-3.067Z"
+                            />
+                        </svg>
+                        <span>Google</span>
+                    </button>
+                    <button
+                        onClick={handleSocialLogin}
+                        className="w-full flex items-center justify-center mb-6 md:mb-0 border border-gray-300 hover:border-mercury-1 hover:bg-gray-200 text-sm text-gray-500 p-3 rounded-lg tracking-wide font-medium cursor-pointer hover:-translate-y-1 transition-all duration-500"
+                    >
+                        <svg
+                            className="mr-2"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                fill="#4A90E2"
+                                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-3 7h-1.924c-.615 0-1.076.252-1.076.889v1.111h3l-.238 3h-2.762v8h-3v-8h-2v-3h2v-1.923c0-2.022 1.064-3.077 3.461-3.077h2.539v3z"
+                            />
+                        </svg>
+                        <span>Facebook</span>
+                    </button>
                 </div>
             </form>
         </GuestLayout>
